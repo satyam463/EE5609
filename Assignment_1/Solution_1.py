d41804af4f68fc44c084fa42d65e144637058196
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-01 21:55:47 +0530

    Updated Code

--- Assignment_1/Solution_1.py
+++ Assignment_1/Solution_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 class point:
   '''
   A class for 3 Dimensional Points
@@ -13,24 +14,20 @@ def check(p1,p2,p3,p4):
   Args : p1,p2,p3,p4 (point objects) where p1,p2 belongs to the first line and p3,p4 belongs to the second line.
   returns: "Parallel", "Perpendicular" and "Neither Parallel nor Perpendicular"
   '''
-  # Direction ratios for Line 1
-  a1 = p2.x - p1.x
-  b1 = p2.y - p1.y
-  c1 = p2.z - p1.z
-  # Direction ratios for Line 2
-  a2 = p4.x - p3.x
-  b2 = p4.y - p3.y
-  c2 = p4.z - p3.z
+  # Direction vector for Line 1
+  A = [p2.x - p1.x, p2.y - p1.y , p2.z - p1.z]
+  # Direction vector for Line 2
+  B = [p4.x - p3.x, p4.y - p3.y , p4.z - p3.z]
 
-  print("Direction Ratios for Line 1: ",a1,b1,c1)
-  print("Direction Ratios for Line 2: ",a2,b2,c2)
+  print("Direction Vector for Line 1: {}i+{}j+{}k".format(A[0],A[1],A[2]))
+  print("Direction Vector for Line 2: {}i+{}j+{}k".format(B[0],B[1],B[2]))
 
-  if (a1/a2==b1/b2 and a1/a2==c1/c2):
-    "Satisfies the condition for being parallel"
-    return "Parallel"
-  elif (a1*a2 + b1*b2 + c1*c2 == 0):
+  if (np.dot(A,B)==0):
     "Satisfies the condition for being perpendicular"
     return "Perpendicular"
+  elif (sum(np.cross(A,B))==0):
+    "Satisfies the condition for being parallel"
+    return "Parallel"
   else:
     "Neither parallel nor perpendicular"
     return "Neither Parallel nor Perpendicular"
@@ -45,4 +42,4 @@ P2 = point(3,4,-2)
 P3 = point(0,3,2)
 P4 = point(3,5,6)
 
-print ("The two lines through the given four points are : {}".format(check(P1,P2,P3,P4)))
+print ("The lines through the given points are : {}".format(check(P1,P2,P3,P4)))
