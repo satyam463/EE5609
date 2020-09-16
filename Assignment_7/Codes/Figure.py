a339e6c6a30e9651688df58325afabc946c93980
Arkadipta De <46627785+Arko98@users.noreply.github.com>
2020-09-16 13:42:26 +0530

    Update Figure.py

--- Assignment_7/Codes/Figure.py
+++ Assignment_7/Codes/Figure.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-5,5,100)
-y_1 = (2*x+7)/3          # Line 1
-y_2 = (20-12*x)/5        # Line 2
+y_1 = (3*x+5)/4          # Line 1
+y_2 = (7-2*x)/3          # Line 2
 
-plt.plot(x, y_1, 'blue', label='2X-3y+7=0')
-plt.plot(x, y_2, 'green', label='12X+5y-20=0')
+plt.plot(x, y_1, 'blue', label='3x-4y+5=0')
+plt.plot(x, y_2, 'green', label='2x+3y-7=0')
 
 plt.xlabel('X - Axis', color='black')
 plt.ylabel('Y - Axis', color='black')
-plt.legend(loc='best')
+plt.legend(loc='upper center')
 plt.grid()
 
 plt.show()
